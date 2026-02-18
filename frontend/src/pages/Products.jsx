911a60aab2bb3390c08b7b4f855d4b12eebce789
Kashi Vishwanath <kashi.vishwanath6254@gmail.com>
2026-02-18 13:47:40 +0530

    Refactor API call to use config for base URL
    Updated the API endpoint to use a base URL from a config file.

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import API_BASE_URL from "../config/api.js";
 // import products from "../json/products.json";
 
 function Products() {
@@ -7,7 +8,7 @@ function Products() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:3000/api/products");
+        const response = await fetch(`${API_BASE_URL}/api/products`);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -101,3 +102,4 @@ function Products() {
 }
 
 export default Products;
+
